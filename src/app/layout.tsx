221ff4ecbc370dd5e9130bf1ae419b2c6d557fe0
Tone Lopes <tonelopes.dev@gmail.com>
2025-10-31 19:31:21 +0000

    continue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;500;700&display=swap" rel="stylesheet" />
         <link rel='icon' href='/favicon.ico' sizes='any' />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body antialiased">
         {children}
