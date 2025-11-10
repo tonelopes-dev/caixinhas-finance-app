b6bc763c9b1a4b24894e3a3eb2707020c62dd39e
tone.monte <tonelopes.dev@gmail.com>
2025-11-10 19:06:53 -0300

    refactor: remover exportações desnecessárias e ajustar comentários no arquivo index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,5 +1,3 @@
-'use client';
-
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
@@ -40,11 +38,5 @@ export function getSdks(firebaseApp: FirebaseApp) {
   };
 }
 
-export * from './provider';
-export * from './client-provider';
-export * from './firestore/use-collection';
-export * from './firestore/use-doc';
-export * from './non-blocking-updates';
-export * from './non-blocking-login';
-export * from './errors';
-export * from './error-emitter';
+// Export apenas o FirebaseClientProvider para uso no layout
+export { FirebaseClientProvider } from './client-provider';
