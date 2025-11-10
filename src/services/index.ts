778812c1528b0fea9fda6c2be688b76e21963f36
tone.monte <tonelopes.dev@gmail.com>
2025-11-10 19:05:47 -0300

    feat: adicionar exportações para AccountService, GoalService e TransactionService

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,4 +5,8 @@ export type { CreateUserInput, LoginInput, UserWithoutPassword } from './auth.se
 export { VaultService } from './vault.service';
 export type { VaultWithMembers, CreateVaultInput, UpdateVaultInput, VaultInvitationData } from './vault.service';
 
+export { AccountService } from './account.service';
+export { GoalService } from './goal.service';
+export { TransactionService } from './transaction.service';
+
 export { default as prisma } from './prisma';
