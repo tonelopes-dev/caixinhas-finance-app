1936a673afe9cd3963b28f387196dce3ecf16e17
Tone Lopes <tonelopes.dev@gmail.com>
2025-11-12 21:48:40 +0000

    studio-5710459563:~/studio{main}$ npx prisma generate Failed to load con

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'prisma/config';
-import { env } from 'process';
 
 export default defineConfig({
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL,
   },
 });
