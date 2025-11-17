301f9ecc896fd515deb6c737637c8e16e547f730
Tone Lopes <tonelopes.dev@gmail.com>
2025-11-17 19:02:04 +0000

    ✓ Compiled /dashboard in 9s [next-auth][warn][DEBUG_ENABLED] https://ne

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -6,10 +6,37 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+// Helper function to add pgbouncer=true if it's not already in the URL
+const getPatchedDatabaseUrl = (): string | undefined => {
+    const url = process.env.DATABASE_URL;
+    if (!url) {
+        return undefined;
+    }
+    const urlParts = url.split('?');
+    if (urlParts.length === 1) {
+        // No query parameters exist, so add pgbouncer
+        return `${url}?pgbouncer=true`;
+    }
+    const [baseUrl, queryParams] = urlParts;
+    if (!queryParams.includes('pgbouncer=true')) {
+        // Query parameters exist, but pgbouncer is not one of them
+        return `${baseUrl}?${queryParams}&pgbouncer=true`;
+    }
+    // pgbouncer is already set
+    return url;
+};
+
+const patchedDatabaseUrl = getPatchedDatabaseUrl();
+
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+    datasources: {
+        db: {
+            url: patchedDatabaseUrl,
+        }
+    }
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
