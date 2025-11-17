1c259dc1b53f62bbc517ee5ebbe7b02eab6f0fa3
Tone Lopes <tonelopes.dev@gmail.com>
2025-11-17 20:03:21 +0000

    percebi que ainda falta para telas 320px

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -57,7 +57,7 @@ export default function Header({ user, partner }: HeaderProps) {
           <Logo className="h-8 w-8" />
           <h1 className="font-headline hidden sm:block text-xl font-bold text-foreground">Caixinhas</h1>
         </Link>
-        <div className="flex items-center gap-2 md:gap-4">
+        <div className="flex items-center gap-1 sm:gap-2 md:gap-4">
           {partner && (
               <TooltipProvider>
                 <Tooltip>
