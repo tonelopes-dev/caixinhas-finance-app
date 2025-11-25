5742af0bcf17d747d869db32c14a8cc39571e10a
Tone Lopes <tonelopes.dev@gmail.com>
2025-11-25 00:11:48 +0000

    fix

--- src/app/(private)/goals/[id]/page.tsx
+++ src/app/(private)/goals/[id]/page.tsx
@@ -9,14 +9,13 @@ import { GoalDetailClient } from '@/components/goals/goal-detail-client';
 
 // A sintaxe do parâmetro foi ajustada para ser compatível com as versões recentes do Next.js
 export default async function GoalDetailPage({ params }: { params: { id: string } }) {
-  const id = params.id;
   const session = await getServerSession(authOptions);
 
   if (!session?.user) {
     redirect('/login');
   }
 
-  const data = await getGoalDetails(id, session.user.id);
+  const data = await getGoalDetails(params.id, session.user.id);
 
   if (!data) {
     notFound();
