cbd756f544c4057eede7c53de85ad38304477747
tone.monte <tonelopes.dev@gmail.com>
2026-01-12 18:10:31 -0300

    refactor: atualizar versão do Service Worker para v20260112-205612

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,11 @@
 /**
  * Service Worker - Caixinhas Finance App
- * Versão: v20260112-175747
- * Build: 2026-01-12T17:57:47.300Z
+ * Versão: v20260112-205612
+ * Build: 2026-01-12T20:56:12.447Z
  * Ambiente: PRODUCTION
  */
 
-const VERSION = 'v20260112-175747';
+const VERSION = 'v20260112-205612';
 const CACHE_NAME = `caixinhas-${VERSION}`;
 const STATIC_CACHE = `caixinhas-static-${VERSION}`;
 const DYNAMIC_CACHE = `caixinhas-dynamic-${VERSION}`;
