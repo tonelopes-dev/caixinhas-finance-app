b8dbab56b2f5e5b2abea463dbb97453e1ff1d0b2
tone.monte <tonelopes.dev@gmail.com>
2026-03-11 17:26:43 -0300

    feat: Create the root layout for the application, including global providers, PWA configuration, analytics, and core UI components.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,8 @@ export default function RootLayout({
         {/* PWA Meta Tags */}
         <meta name="application-name" content="Caixinhas" />
         <meta name="theme-color" content="#f4efe7" />
+
+       
         
         {/* iOS Safari PWA Meta Tags */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
@@ -72,6 +74,34 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '420868668647496');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=420868668647496&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body>
         <NextAuthProvider>
